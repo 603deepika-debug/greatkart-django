ef77f02b17ac4a4a7413c92ee32f15aa4173d93e
Deepika <603deepika@gmail.com>
2026-04-11 22:40:12 +0530

    django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -131,3 +131,17 @@ STATICFILES_DIRS = [
 
 MEDIA_URL='/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR:'danger',
+}
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = '603deepika@gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST_PASSWORD = 'deepika8077'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'greatkart <603deepika@gmail.com>'
